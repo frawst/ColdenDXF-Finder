1ff576560352821e66d81b444d65e1fb048ac56c
frawst <frawst@live.ca>
2023-08-24 17:20:31 -0600

    init commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from cx_Freeze import setup, Executable
+
+setup(
+    name="Find_dxf_2",
+    version="0.1",
+    description="My Description",
+    executables=[Executable("Find_dxf_2.py")]
+    
+)
